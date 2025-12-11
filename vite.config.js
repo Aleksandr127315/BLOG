610c463c0a05c60024f1c4d0b257d6e8c341bcf9
Aleksandr Merzlikin <merzlikina730@gmail.com>
2025-12-11 16:26:31 +0300

    создать BFF

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const isDev = mode !== "production";
+	const isDev = mode !== 'production';
 
-  return {
-    plugins: [
-      react({
-        babel: {
-          plugins: isDev ? ["check-prop-types"] : [],
-        },
-      }),
-    ],
-  };
+	return {
+		plugins: [
+			react({
+				babel: {
+					plugins: isDev ? ['check-prop-types'] : [],
+				},
+			}),
+		],
+	};
 });
-
