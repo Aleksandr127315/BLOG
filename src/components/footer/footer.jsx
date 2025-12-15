73757496c7927d44b4131ac8dc22baf15301d1a4
Aleksandr Merzlikin <merzlikina730@gmail.com>
2025-12-15 11:31:35 +0300

    реализовали footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+
+const FooterContainer = ({ className }) => {
+	const [city, setSity] = useState('');
+	const [temperature, setTemperature] = useState('');
+	const [weather, setWeather] = useState('');
+
+	useEffect(() => {
+		fetch(
+			'https://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric&lang=ru&appid=e1802e00fb58d6feb5545a5c38bb53d3',
+		)
+			.then((res) => res.json())
+			.then(({ name, main, weather }) => {
+				setSity(name);
+				setTemperature(Math.round(main.temp));
+				setWeather(weather[0].description);
+			});
+	}, []);
+
+	return (
+		<div className={className}>
+			<div>
+				<div>Блог веб-разработчика</div>
+				<div>merzlikin730@gmail.com</div>
+			</div>
+			<div>
+				<div>
+					{city},{' '}
+					{new Date().toLocaleString('ru', { day: 'numeric', month: 'long' })}
+				</div>
+				{temperature} градусов, {weather}
+			</div>
+		</div>
+	);
+};
+
+export const Footer = styled(FooterContainer)`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 1000px;
+	height: 120px;
+	padding: 20px 40px;
+	font-weight: bold;
+	background-color: #fff;
+	box-shadow: 0 2px 17px #000;
+`;
